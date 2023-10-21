e0757e7240460d46dcc0f6d590e2197820461c98
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-10-21 12:35:21 +0100

    Create Custom Exception

--- src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -24,17 +24,17 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
     @Override
     public Employee getEmployeeById(long id) {
-        // Optional<Employee> employee = employeeRepository.findById(id);
-        // if(employee.isPresent()) {
-        // return employee.get();
-        // }else {
-        // throw new ResourceNotFoundException("Employee", "Id", id);
-        // }
+        //  Optional<Employee> employee = employeeRepository.findById(id);
+        //  if(employee.isPresent()) {
+        //      return employee.get();
+        //  }else {
+        //      throw new ResourceNotFoundException("Employee", "Id", id);
+        //  }
         return employeeRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Employee", "Id", id));
     }
     @Override
     public Employee updateEmployee(Employee employee, long id) {
-        // we need to check whether employee with given id is exist in DB or not
+        // we need to check whether employee with given id exists in DB or not
         Employee existingEmployee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", id));
         existingEmployee.setFirstName(employee.getFirstName());
         existingEmployee.setLastName(employee.getLastName());
